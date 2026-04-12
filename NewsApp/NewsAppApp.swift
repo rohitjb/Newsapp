b017486a1170ac13c8d01000e1d34086aa09abbd
Rohit <rohit@thebrightapp.xyz>
2026-04-12 13:04:06 +1000

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Rohit on 12/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsAppTests/NewsAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  NewsAppTests.swift
+//  NewsAppTests
+//
+//  Created by Rohit on 12/4/2026.
+//
+
+import Testing
+@testable import NewsApp
+
+struct NewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
